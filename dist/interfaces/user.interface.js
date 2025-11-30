2c12c97c3c92df7f2a02cf364df5e856a715917d
ArnabShikder24 <arnabshikder2001@gmail.com>
2025-12-01 02:34:37 +0600

    ingore update

--- dist/interfaces/user.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Role = void 0;
-var Role;
-(function (Role) {
-    Role["admin"] = "admin";
-    Role["moderator"] = "moderator";
-    Role["member"] = "member";
-})(Role || (exports.Role = Role = {}));
